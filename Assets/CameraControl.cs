5bfbf6325af9f75d2292cac7345e7178834cfa53
Bruno De Maeyer <brunodemaeyer.it@gmail.com>
2018-03-04 18:32:26 +0100

    Initial upload to Github

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject Player;
+    public Vector3 CameraOffset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        CameraOffset = transform.position - Player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Player.transform.position + CameraOffset;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rbPlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rbPlayer = GetComponent<Rigidbody>();	
+	}
+	
+
+	void FixedUpdate ()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
+        float yMovement = Input.GetAxis("Fire1");
+
+        Vector3 move = new Vector3(xMovement, yMovement, zMovement);
+        rbPlayer.AddForce(move*speed);
+	}
+}
